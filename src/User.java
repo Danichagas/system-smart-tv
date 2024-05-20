817ca8a7531d3cef70832b1ef57abed6241de573
Dani Chagas <chagasdaniel788@gmail.com>
2024-05-20 16:16:44 -0300

    sysout test

--- src/User.java
+++ src/User.java
@@ -1,5 +1,10 @@
 public class User {
   public static void main(String[] args) {
     
+    SmartTv smartTv = new SmartTv();
+
+    System.out.println("A TV está ligada? : " + smartTv.on);
+    System.out.println("Canal atual : " + smartTv.channel);
+    System.out.println("Volume atual : " + smartTv.volume);
   }
 }
