671dd2cd22667d9c743c20da6b5241439a7ce463
Dani Chagas <chagasdaniel788@gmail.com>
2024-05-21 01:07:05 -0300

    I added new interactions with the user, such as changing the channel and showing the new status in the console

--- src/User.java
+++ src/User.java
@@ -3,8 +3,21 @@ public class User {
     
     SmartTv smartTv = new SmartTv();
 
+    smartTv.lowerVolume();
+    smartTv.lowerVolume();
+    smartTv.lowerVolume();
+    smartTv.turnUpVolume();
+
+    System.out.println("Canal atual : " + smartTv.channel);
+
+    smartTv.changeChannel(22);
+    System.out.println("Canal atual : " + smartTv.channel);
+
     System.out.println("A TV está ligada? : " + smartTv.on);
     System.out.println("Canal atual : " + smartTv.channel);
     System.out.println("Volume atual : " + smartTv.volume);
+
+    smartTv.turnOnTv();
+    System.out.println("Novo estatus : --> A TV está ligada? : " + smartTv.on);
   }
 }
